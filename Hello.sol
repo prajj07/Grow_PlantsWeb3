4e020f82e93819a7a56e18676c33ee94b5885290
Prajwal <83826853+Prajwal813793@users.noreply.github.com>
2023-02-08 22:31:47 +0530

    test

---
+++ Hello.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// install
+//compile
+//run ganache (deploy + demo)
+// debugger
+contract HelloWorld {
+  string public greet = "Hello World!";
+
+  address public sender;
+  uint public amount;
+
+  function pay() external payable {
+    sender = msg.sender;
+    amount= msg.value;
+  }
+
+  function fail() external {
+    require (false, "FAILED HERE");
+    greet = "";
+  }
+}
